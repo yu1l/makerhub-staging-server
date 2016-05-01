97c37a63fc1a0e51d1f26834afc3a9d021088f60
Yu Hoshino <yhoshino11@gmail.com>
2016-05-02 01:02:49 +0900

    show duration better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def time(seconds)
+    hour = seconds / 3600
+    min = (seconds - 3600 * hour) / 60
+    sec = seconds - 3600 * hour - min * 60
+    if hour > 0
+      return "#{hour} #{I18n.t('time.hour')} #{min} #{I18n.t('time.min')} #{sec} #{I18n.t('time.sec')}"
+    elsif min > 0
+      return "#{min} #{I18n.t('time.min')} #{sec} #{I18n.t('time.sec')}"
+    else
+      return "#{sec} #{I18n.t('time.sec')}"
+    end
+  end
 end
