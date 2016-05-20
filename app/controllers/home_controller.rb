53547efb6ac91e60dbd377f64601676cdedaa7c3
yhoshino11 <yhoshino11@gmail.com>
2016-05-21 04:47:55 +0900

    set locale from browser information

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,19 @@
-# Root
 class HomeController < ApplicationController
+  before_action :set_locale
+
   def index
     @users = User.where(live: true)
+  end
+
+  def set_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+
+  private
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
