0eb6eca99bd65bd5ee0481bac7684bfbb3de18a8
yhoshino11 <yhoshino11@gmail.com>
2016-05-22 01:58:46 +0900

    add spec for Records#play

--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -21,4 +21,42 @@
 require 'rails_helper'
 
 RSpec.describe RecordsController, type: :controller do
+  describe 'GET #play' do
+    context 'invalid' do
+      context 'record does not exist' do
+        before do
+          @user = User.find_from_auth(github_hash, nil)
+          @record = @user.records.create(title: @user.title, total: @user.total)
+        end
+
+        it do
+          get :play, name: @user.name, uuid: ''
+          expect(response).to redirect_to(stream_path(name: @user.name))
+        end
+      end
+
+      context 'user does not exist' do
+        before do
+          @user = User.find_from_auth(github_hash, nil)
+          @record = @user.records.create(title: @user.title, total: @user.total)
+        end
+
+        it do
+          get :play, name: '', uuid: @record.uuid
+          expect(response).to redirect_to(root_path)
+        end
+      end
+    end
+    context 'valid' do
+      before do
+        @user = User.find_from_auth(github_hash, nil)
+        @record = @user.records.create(title: @user.title, total: @user.total)
+      end
+
+      it do
+        get :play, name: @user.name, uuid: @record.uuid
+        expect(response).to render_template(:play)
+      end
+    end
+  end
 end
