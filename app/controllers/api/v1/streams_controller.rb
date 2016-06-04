5e8d297509ceef25bb45955d01085af0b546740a
yhoshino11 <yhoshino11@gmail.com>
2016-06-04 09:56:41 +0900

    done GET /api/v1/streams

--- app/controllers/api/v1/streams_controller.rb
+++ app/controllers/api/v1/streams_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::StreamsController < Api::V1::ApiController
+  def all
+    @users = User.where(live: true).map do |u|
+      {
+        thumbnail: "http://makerhub.live/screenshot/#{u.gh.nickname}.png",
+        title: u.title,
+        summary: u.description,
+        viewers: u.total,
+        category: u.category_in_text,
+        user: {
+          name: u.name,
+          nickname: u.gh.nickname,
+          avatar: u.gh.image
+        }
+      }
+    end
+
+    render json: { streams: @users }
+  end
 end
--- spec/controllers/api/v1/streams_controller_spec.rb
+++ spec/controllers/api/v1/streams_controller_spec.rb
@@ -2,7 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::StreamsController, type: :controller do
   describe 'GET #all' do
+    let(:user) { User.find_from_auth(github_hash, nil) }
+    before do
+      user.update(live: true)
+      get :all, format: :json
+      expect(response).to be_success
+    end
 
+    it do
+      parsed_response = JSON.parse(response.body)['streams']
+      sample = parsed_response[0]
+      expect(sample['title']).not_to be_nil
+      expect(sample['thumbnail']).not_to be_nil
+      expect(sample['summary']).not_to be_nil
+      expect(sample['viewers']).not_to be_nil
+      expect(sample['category']).not_to be_nil
+      expect(sample['user']['name']).not_to be_nil
+      expect(sample['user']['nickname']).not_to be_nil
+      expect(sample['user']['avatar']).not_to be_nil
+    end
   end
 
   describe 'GET #user' do
