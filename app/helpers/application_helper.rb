82e920dfb0be8ef0f4e7bf0426888f9853fe2fa0
yhoshino11 <yhoshino11@gmail.com>
2016-06-12 16:02:04 +0900

    refactored applcation_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def stream_url
     return 'makerhub.live' if Rails.env.production?
-    return '192.168.179.2' if Rails.env.development?
+    '192.168.179.2'
   end
 
   def category(index)
@@ -14,15 +14,35 @@ module ApplicationHelper
   end
 
   def time(seconds)
-    hour = seconds / 3600
-    min = (seconds - 3600 * hour) / 60
-    sec = seconds - 3600 * hour - min * 60
-    if hour > 0
-      return "#{hour} #{I18n.t('time.hour')} #{min} #{I18n.t('time.min')} #{sec} #{I18n.t('time.sec')}"
-    elsif min > 0
-      return "#{min} #{I18n.t('time.min')} #{sec} #{I18n.t('time.sec')}"
-    else
-      return "#{sec} #{I18n.t('time.sec')}"
+    Duration.new(seconds).time_in_str
+  end
+
+  class Duration
+    attr_accessor :hour, :min, :sec
+
+    def initialize(seconds)
+      @hour = seconds / 3600
+      @min = (seconds - 3600 * @hour) / 60
+      @sec = seconds - 3600 * @hour - @min * 60
+    end
+
+    def time_in_str
+      "#{hour_to_str}#{min_to_str}#{sec_to_str}"
+    end
+
+    private
+
+    def hour_to_str
+      hour > 0 ? "#{hour} #{I18n.t('time.hour')} " : ''
+    end
+
+    def min_to_str
+      return "#{min} #{I18n.t('time.min')} " if hour > 0
+      min > 0 ? "#{min} #{I18n.t('time.min')} " : ''
+    end
+
+    def sec_to_str
+      "#{sec} #{I18n.t('time.sec')}"
     end
   end
 end
