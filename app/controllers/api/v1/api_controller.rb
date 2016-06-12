1a740d2648899b69a0dbb19cc177c3a0fad1e705
yhoshino11 <yhoshino11@gmail.com>
2016-06-12 18:21:28 +0900

    refactored api/v1/api_controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,10 @@
-class Api::V1::ApiController < ActionController::Base
-  include Pundit
-  protect_from_forgery with: :null_session
-  before_action :doorkeeper_authorize!
-  respond_to    :json
+module Api
+  module V1
+    class ApiController < ActionController::Base
+      include Pundit
+      protect_from_forgery with: :null_session
+      before_action :doorkeeper_authorize!
+      respond_to    :json
+    end
+  end
 end
