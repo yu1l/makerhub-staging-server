10d45863fd369ebbb2d2d331eed8b5b421625fe8
yhoshino11 <yhoshino11@gmail.com>
2016-06-13 16:50:03 +0900

    refactored channels_controller

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -3,17 +3,24 @@ class ChannelsController < ApplicationController
   include Contracts::Builtin
 
   before_action :authenticate_user!
+  rescue_from Exception, with: :respond_with_500
 
   def block
     @user = User.find_by(user_params)
+    authorize(@user, :other?)
     current_user.channel.block(@user)
     render nothing: true, status: 200
   end
 
   def unblock
     @user = User.find_by(user_params)
+    authorize(@user, :other?)
     current_user.channel.unblock(@user)
     render nothing: true, status: 200
+  end
+
+  def respond_with_500
+    render nothing: true, status: 500
   end
 
   private
--- spec/controllers/channels_controller_spec.rb
+++ spec/controllers/channels_controller_spec.rb
@@ -17,32 +17,41 @@
 require 'rails_helper'
 
 RSpec.describe ChannelsController, type: :controller do
-  describe 'block' do
-    before do
-      @user = User.find_from_auth(github_hash, nil)
-      sign_in(@user)
-      @other = create(:user, nickname: Faker::Internet.user_name)
+  before do
+    @user = User.find_from_auth(github_hash, nil)
+    sign_in(@user)
+    @other = create(:user, nickname: Faker::Internet.user_name)
+  end
+
+  describe 'POST #block' do
+    context 'valid params' do
+      it { expect { post :block, user: { nickname: @other.nickname } }.to change { @user.channel.blocks }.from([]).to([@other]) }
     end
 
-    it do
-      expect {
-        post :block, user: { nickname: @other.nickname }
-      }.to change{@user.channel.blocks}.from([]).to([@other])
+    context 'invalid params' do
+      it { expect { post :block, user: { nickname: '' } }.not_to change { @user.channel.blocks }.from([]) }
+    end
+
+    context 'user can not block himself' do
+      it { expect { post :block, user: { nickname: @user.nickname } }.not_to change { @user.channel.blocks }.from([]) }
     end
   end
 
   describe 'unblock' do
     before do
-      @user = User.find_from_auth(github_hash, nil)
-      sign_in(@user)
-      @other = create(:user, nickname: Faker::Internet.user_name)
       @user.channel.block(@other)
     end
 
-    it do
-      expect {
-        post :unblock, user: { nickname: @other.nickname }
-      }.to change{@user.channel.blocks}.from([@other]).to([])
+    context 'valid params' do
+      it { expect { post :unblock, user: { nickname: @other.nickname } }.to change { @user.channel.blocks }.from([@other]).to([]) }
+    end
+
+    context 'invalid params' do
+      it { expect { post :unblock, user: { nickname: '' } }.not_to change { @user.channel.blocks }.from([@other]) }
+    end
+
+    context 'user can not unblock himself' do
+      it { expect { post :unblock, user: { nickname: @user.nickname } }.not_to change { @user.channel.blocks }.from([@other]) }
     end
   end
 end
